1655fad0be43ccedd91597abb845e30db92fc715
Arie Trouw <arie.trouw@xy.company>
2019-10-13 15:19:56 -0700

    Defaults Set

--- xyo-android-library/src/main/java/network/xyo/sdk/XyoNode.kt
+++ xyo-android-library/src/main/java/network/xyo/sdk/XyoNode.kt
@@ -1,7 +1,8 @@
 package network.xyo.sdk
 
 import network.xyo.base.XYBase
+import network.xyo.sdkcorekotlin.persist.XyoKeyValueStore
 
-class XyoNode(val storage: XyoStorage, val networks: Array<XyoNetwork>): XYBase() {
+class XyoNode(val storage: XyoKeyValueStore, val networks: Array<XyoNetwork>): XYBase() {
 
 }
--- xyo-android-library/src/main/java/network/xyo/sdk/XyoNodeBuilder.kt
+++ xyo-android-library/src/main/java/network/xyo/sdk/XyoNodeBuilder.kt
@@ -2,18 +2,25 @@ package network.xyo.sdk
 
 import android.content.Context
 import network.xyo.base.XYBase
+import network.xyo.sdkcorekotlin.hashing.XyoBasicHashBase
 import network.xyo.sdkcorekotlin.hashing.XyoHash
 import network.xyo.sdkcorekotlin.network.XyoProcedureCatalog
+import network.xyo.sdkcorekotlin.network.XyoProcedureCatalogFlags
 import network.xyo.sdkcorekotlin.node.XyoRelayNode
+import network.xyo.sdkcorekotlin.persist.XyoKeyValueStore
+import network.xyo.sdkcorekotlin.persist.repositories.XyoStorageBridgeQueueRepository
+import network.xyo.sdkcorekotlin.persist.repositories.XyoStorageOriginBlockRepository
+import network.xyo.sdkcorekotlin.persist.repositories.XyoStorageOriginStateRepository
 import network.xyo.sdkcorekotlin.repositories.XyoBridgeQueueRepository
 import network.xyo.sdkcorekotlin.repositories.XyoOriginBlockRepository
 import network.xyo.sdkcorekotlin.repositories.XyoOriginChainStateRepository
+import network.xyo.sdkcorekotlin.schemas.XyoSchemas
 import java.lang.Exception
 
 @kotlin.ExperimentalUnsignedTypes
 class XyoNodeBuilder: XYBase() {
     private var networks = mutableListOf<XyoNetwork>()
-    private var storage: XyoStorage? = null
+    private var storage: XyoKeyValueStore? = null
 
     private var relayNode: XyoRelayNode? = null
     private var procedureCatalog: XyoProcedureCatalog? = null
@@ -26,7 +33,7 @@ class XyoNodeBuilder: XYBase() {
         networks.add(network)
     }
 
-    fun setStorage(storage: XyoStorage) {
+    fun setStorage(storage: XyoKeyValueStore) {
         this.storage = storage
     }
 
@@ -35,9 +42,14 @@ class XyoNodeBuilder: XYBase() {
             throw Exception()
         }
 
-        if (networks.isEmpty()) {
-            log.info("No networks specified, using default")
-            setDefaultNetworks(context)
+        if (storage == null) {
+            log.info("No storage specified, using default")
+            setDefaultStorage(context)
+        }
+
+        if (hashingProvider == null) {
+            log.info("No hashingProvider specified, using default")
+            setDefaultHashingProvider()
         }
 
         if (blockRepository == null) {
@@ -55,11 +67,6 @@ class XyoNodeBuilder: XYBase() {
             setDefaultBridgeQueueRepository()
         }
 
-        if (hashingProvider == null) {
-            log.info("No hashingProvider specified, using default")
-            setDefaultHashingProvider()
-        }
-
         if (procedureCatalog == null) {
             log.info("No procedureCatalog specified, using default")
             setDefaultProcedureCatalog()
@@ -70,9 +77,9 @@ class XyoNodeBuilder: XYBase() {
             setDefaultRelayNode()
         }
 
-        if (storage == null) {
-            log.info("No storage specified, using default")
-            setDefaultStorage()
+        if (networks.isEmpty()) {
+            log.info("No networks specified, using default")
+            setDefaultNetworks(context)
         }
 
         val node = XyoNode(storage!!, networks.toTypedArray())
@@ -81,40 +88,113 @@ class XyoNodeBuilder: XYBase() {
     }
 
     private fun setDefaultProcedureCatalog() {
+        procedureCatalog = object : XyoProcedureCatalog {
+            val canDoByte = XyoProcedureCatalogFlags.BOUND_WITNESS or XyoProcedureCatalogFlags.GIVE_ORIGIN_CHAIN or XyoProcedureCatalogFlags.TAKE_ORIGIN_CHAIN
 
+            override fun canDo(byteArray: ByteArray): Boolean {
+                if (byteArray.isEmpty()) {
+                    return false
+                }
+
+                return byteArray.last().toInt() and canDoByte != 0
+            }
+
+            override fun choose(byteArray: ByteArray): ByteArray {
+                if (byteArray.isEmpty()) {
+                    return byteArrayOf(XyoProcedureCatalogFlags.BOUND_WITNESS.toByte())
+                }
+
+                val interestedIn = byteArray.last().toInt()
+
+                if (interestedIn and XyoProcedureCatalogFlags.GIVE_ORIGIN_CHAIN != 0) {
+                    return byteArrayOf(XyoProcedureCatalogFlags.TAKE_ORIGIN_CHAIN.toByte())
+                }
+
+                if (interestedIn and XyoProcedureCatalogFlags.TAKE_ORIGIN_CHAIN != 0) {
+                    return byteArrayOf(XyoProcedureCatalogFlags.GIVE_ORIGIN_CHAIN.toByte())
+                }
+
+                return byteArrayOf(XyoProcedureCatalogFlags.BOUND_WITNESS.toByte())
+            }
+
+            override fun getEncodedCanDo(): ByteArray {
+
+                return byteArrayOf(0x00, 0x00, 0x00, canDoByte.toByte())
+            }
+        }
     }
 
     private fun setDefaultBlockRepository() {
-
+        storage?.let { storage ->
+            hashingProvider?.let {hashingProvider ->
+                blockRepository = XyoStorageOriginBlockRepository(storage, hashingProvider)
+                return
+            }
+            log.error("Missing hashingProvider", true)
+            return
+        }
+        log.error("Missing storage", true)
     }
 
     private fun setDefaultStateRepository() {
-
+        storage?.let { storage ->
+            stateRepository = XyoStorageOriginStateRepository(storage)
+            return
+        }
+        log.error("Missing storage", true)
     }
 
     private fun setDefaultBridgeQueueRepository() {
-
+        storage?.let { storage ->
+            bridgeQueueRepository = XyoStorageBridgeQueueRepository(storage)
+            return
+        }
+        log.error("Missing storage", true)
     }
 
     private fun setDefaultHashingProvider() {
-
+        hashingProvider = XyoBasicHashBase.createHashType(XyoSchemas.SHA_256, "SHA-256")
     }
 
     private fun setDefaultRelayNode() {
-        relayNode = XyoRelayNode(
-            blockRepository!!,
-            stateRepository!!,
-            bridgeQueueRepository!!,
-            hashingProvider!!
-        )
+        blockRepository?.let {blockRepository ->
+            stateRepository?.let { stateRepository ->
+                bridgeQueueRepository?.let { bridgeQueueRepository ->
+                    hashingProvider?.let { hashingProvider ->
+                        relayNode = XyoRelayNode(
+                            blockRepository,
+                            stateRepository,
+                            bridgeQueueRepository,
+                            hashingProvider
+                        )
+                        return
+                    }
+                    log.error("Missing hashingProvider", true)
+                    return
+                }
+                log.error("Missing bridgeQueueRepository", true)
+                return
+            }
+            log.error("Missing stateRepository", true)
+            return
+        }
+        log.error("Missing blockRepository", true)
     }
 
     private fun setDefaultNetworks(context: Context) {
-        addNetwork(XyoBleNetwork(context, relayNode!!, procedureCatalog!!))
-        addNetwork(XyoTcpIpNetwork(relayNode!!, procedureCatalog!!))
+        relayNode?.let {relayNode ->
+            procedureCatalog?.let { procedureCatalog ->
+                addNetwork(XyoBleNetwork(context, relayNode, procedureCatalog))
+                addNetwork(XyoTcpIpNetwork(relayNode, procedureCatalog))
+                return
+            }
+            log.error("Missing procedureCatalog", true)
+            return
+        }
+        log.error("Missing relayNode", true)
     }
 
-    private fun setDefaultStorage() {
-        setStorage(XyoStorage())
+    private fun setDefaultStorage(context: Context) {
+        storage = XyoSnappyDbStorageProvider(context)
     }
 }
